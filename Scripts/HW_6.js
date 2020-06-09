6db524ec76a62b5f25bdabf3798843d81a31df2f
Cosya007 <52891677+Cosya@users.noreply.github.com>
2020-06-10 00:08:10 +0300

    Create HW_6.js

---
+++ Scripts/HW_6.js
@@ -0,0 +1,65 @@
+//HW_6 1
+let user={} ;
+console.log(user);
+user.name ='John';
+console.log(user);
+user.surname = 'smith';
+console.table(user)
+user.name = 'peter'
+console.table(user)
+;
+
+//HW_6 2
+
+const users = [
+  {name: 'Ivan', age: 35, gender: 'male'},
+  {name: 'Ann', age: 33, gender: 'female'},
+  {name: 'Jane', age: 40, gender: 'female'},
+  {name: 'Alex', age: 20, gender: 'male'},
+  {name: 'Sam', age: 52, gender: 'female'}
+];
+
+function getObjectAverageAge(users)
+  {
+    let ages = users.map(user => user.age);
+     result = ages.reduce((sum,current) => sum+ +current, 0) /ages.length;
+    return result;
+  }
+
+console.log(getObjectAverageAge(users))
+
+//HW_6 3
+
+let user = {name: 'Ivan', age: 30, gender: 'male'};
+//let user = {};
+function isEmpty(obj) 
+{
+  let isEmpty ;
+  let keyArray = Object.keys(user);
+    if (keyArray[0] === undefined) {
+      isEmpty = true;
+    }
+    else {  
+   isEmpty = false;
+    }
+    return isEmpty;
+  }
+ 
+console.log(isEmpty(user));
+
+//HW_6 4
+let user = {name: 'Ivan', age: 30, size: 15, gender: 'male'};
+
+function multiplyNumeric(obj) 
+{
+  for (let key in obj) {
+    //console.log(typeof obj[key])
+    if (typeof obj[key]=='number'){
+      obj[key] = obj[key]*2;
+    }
+   
+  }
+  
+}
+multiplyNumeric(user);
+console.table(user);
