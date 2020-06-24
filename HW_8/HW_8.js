5f1ffeecc8230b8009740208200f60d7d7cc0cb9
Cosya007 <52891677+Cosya@users.noreply.github.com>
2020-06-24 13:28:18 +0300

    8 finish
    8 finish

--- HW_8/HW_8.js
+++ HW_8/HW_8.js
@@ -1,32 +1,53 @@
+//HW_8 1-2
+
+function addStyle(element,evenClassName, oddstyle) {
+  let currentElement = document.getElementById(element)
+  console.log(currentElement.children);
+
+  for (let i = 0; i < currentElement.children.length; i++) {
+    console.log(currentElement.children[i]);
+
+    if (i%2 == 0) {
+      currentElement.children[i].classList.add(evenClassName)
+    }
+    else {
+      currentElement.children[i].style = oddstyle ;
+    }
+
+  }
+}
+
+addStyle('grid-1','grid-item-background','background: #aff0ad;')
+
 //HW  3
 function toggleClass(element, className) {
   let currentElement = document.getElementById(element)
-  console.log(currentElement.children) ;
-  
-//  for (let i = 0; i < currentElement.children.length; i++) 
-  //{ 
-     console.log(currentElement.classList.contains(className));
+  console.log(currentElement.children);
 
-      if (currentElement.classList.contains(className)) {
-        currentElement.classList.remove(className)
-        }
-      else {
-        currentElement.classList.add(className)
-        }
-    
+  //  for (let i = 0; i < currentElement.children.length; i++) 
+  //{ 
+  console.log(currentElement.classList.contains(className));
+
+  if (currentElement.classList.contains(className)) {
+    currentElement.classList.remove(className)
+  }
+  else {
+    currentElement.classList.add(className)
+  }
+
   //} 
   return currentElement;
- }
-console.log(toggleClass( 'grid-1','grid-item-background'))
+}
+toggleClass('grid-1', 'grid-item-background')
 
 //HW  4
-  let userText = prompt('Enter any text here:');
-  const  currentClass = '.text-placeholder';
-  let currentElement = document.querySelector(currentClass);
-  console.log(currentElement);
-  currentElement.innerText = userText;
-  console.log(currentElement);
-  
+let userText = prompt('Enter any text here:');
+const currentClass = '.text-placeholder';
+let currentElement = document.querySelector(currentClass);
+console.log(currentElement);
+currentElement.innerText = userText;
+console.log(currentElement);
+
 
 
 
